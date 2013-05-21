1840636a6d3478b30fe7a75f57d64b690eff68cc
João Taveira Araújo <joao.taveira@gmail.com>
2013-05-21 22:52:45 +0100

    Reformatted config.

--- scrapefm/config.py
+++ scrapefm/config.py
@@ -1,30 +1,31 @@
-options = { 	
-		# Maximum number of friends to collect per user. The last.fm API
-                # can only retrieve up to 200 friends at a time, so this value 
-                # determines how long we spend scraping a user.
-                'maxfriends': 1000,
+options = { 
+    # Maximum number of friends to collect per user. The last.fm API
+    # can only retrieve up to 200 friends at a time, so this value 
+    # determines how long we spend scraping a user.
+    'maxfriends': 1000,
 
-                # Delay between requests. Requires pylast from
-                # 'delay': 0
+    # Delay between requests. Recommended is 1s.
+    # Requires pylast from https://github.com/jta/pylast.git
+    # 'delay': 0
 
-                # Export friendship relationships. If skipped, we only query friend 
-                # list in order to populate random walk.
-                'do_connect': False,
+    # Export friendship relationships. If skipped, we only query friend 
+    # list in order to populate random walk.
+    'do_connect': False,
 
-                # Dates for which to crawl data. Datematch provides the desired
-                # period in string format, while datefmt describes how that format
-                # should be converted to the native date representation.
-                'datematch': "2013-01",
-                'datefmt': "%Y-%m",
+    # Dates for which to crawl data. Datematch provides the desired
+    # period in string format, while datefmt describes how that format
+    # should be converted to the native date representation.
+    'datematch': "2013-01",
+    'datefmt': "%Y-%m",
 
-                # Username to start random walk at.
-                'userseed': 'RJ',
+    # Username to start random walk at.
+    'userseed': 'RJ',
 
-                # Maximum number of users to scrape.
-                'limit': 1000,
+    # Maximum number of users to scrape.
+    'limit': 1000,
 
-                # Number seed for random, used for selecting next user in random walk.
-                'numseed': 666 
+    # Seed for random, used for selecting next user in random walk.
+    'numseed': 666 
 }
 
 
